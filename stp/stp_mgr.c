06ab1d9a97105ef52112ca01610c3dad3f5cae1d
Divya Kumaran Chandralekha <66686927+divyachandralekha@users.noreply.github.com>
2025-07-04 11:41:07 +0530

    Update stp_mgr.c

--- stp/stp_mgr.c
+++ stp/stp_mgr.c
@@ -309,9 +309,6 @@ void stpmgr_disable_port(STP_CLASS *stp_class, PORT_ID port_number)
 		stptimer_stop(&stp_class->tcn_timer);
 		config_bpdu_generation(stp_class);
 		stptimer_start(&stp_class->hello_timer, 0);
-
-		stplog_topo_change(stp_class, port_number, STP_DISABLE_PORT);
-		stplog_new_root(stp_class, STP_DISABLE_PORT);
 	}
 }
 
